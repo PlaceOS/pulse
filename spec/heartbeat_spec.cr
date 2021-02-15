c38e19c9c09565221673489e30040448d12c8bb8
GabFitzgerald <gabfitzgeralddeveloper@gmail.com>
2021-02-15 13:56:38 +1000

    spec: redundant heartbeat specs removed

--- spec/heartbeat_spec.cr
+++ spec/heartbeat_spec.cr
@@ -6,8 +6,5 @@ describe Pulse::Heartbeat do
 
     heartbeat.instance_type.should eq "production"
     heartbeat.staff_api.should eq true
-    heartbeat.drivers_qty.should be_a Int32
-    heartbeat.users_qty.should be_a Int32
-    heartbeat.zones_qty.should be_a Int32
   end
 end
