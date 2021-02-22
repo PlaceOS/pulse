3d9d664ade66fec53f8eed619f3f33f593489581
GabFitzgerald <gabfitzgeralddeveloper@gmail.com>
2021-02-22 17:47:18 +1000

    ref: renamed message.message to message.contents and revised specs to reflect

--- spec/pulse_spec.cr
+++ spec/pulse_spec.cr
@@ -26,13 +26,13 @@ describe Message do
     message.portal_uri.should eq URI.parse "http://placeos.run"
     message.signature.should eq "7c51c1bf33cec94259a53f6a5d72420af6d83be0dc16824cf0739df1c844c6c3554597f8ca8739e018bd31c1ba4652215572fcd7ac33ea6d72c0167bd7ab1b0f"
 
-    secret.public_key.verify_detached(message.message.to_json, message.signature.hexbytes).should be_nil
+    secret.public_key.verify_detached(message.contents.to_json, message.signature.hexbytes).should be_nil
   end
 
   it ".payload" do
     secret = Sodium::Sign::SecretKey.new("b18e1d0045995ec3d010c387ccfeb984d783af8fbb0f40fa7db126d889f6dadd77f48b59caeda77751ed138b0ec667ff50f8768c25d48309a8f386a2bad187fb".hexbytes)
     message = Message.new("01EY4PBEN5F999VQKP55V4C3WD", secret)
-    message.payload.should eq "{\"instance_id\":\"01EY4PBEN5F999VQKP55V4C3WD\",\"message\":{\"drivers_qty\":0,\"zones_qty\":0,\"users_qty\":0,\"staff_api\":true,\"instance_type\":\"production\"},\"signature\":\"7c51c1bf33cec94259a53f6a5d72420af6d83be0dc16824cf0739df1c844c6c3554597f8ca8739e018bd31c1ba4652215572fcd7ac33ea6d72c0167bd7ab1b0f\"}"
+    message.payload.should eq "{\"instance_id\":\"01EY4PBEN5F999VQKP55V4C3WD\",\"contents\":{\"drivers_qty\":0,\"zones_qty\":0,\"users_qty\":0,\"staff_api\":true,\"instance_type\":\"production\"},\"signature\":\"7c51c1bf33cec94259a53f6a5d72420af6d83be0dc16824cf0739df1c844c6c3554597f8ca8739e018bd31c1ba4652215572fcd7ac33ea6d72c0167bd7ab1b0f\"}"
   end
 
   it ".send" do
--- src/pulse.cr
+++ src/pulse.cr
@@ -34,21 +34,21 @@ end
 class Message < Pulse
   include JSON::Serializable
   getter instance_id : String
-  getter message : Pulse::Heartbeat # rename to payload
+  getter contents : Pulse::Heartbeat # revise type, make generic
   getter signature : String
   getter portal_uri : URI
 
   def initialize(
     @instance_id : String,
     secret_key : Sodium::Sign::SecretKey,
-    @message = Pulse::Heartbeat.new,
+    @contents = Pulse::Heartbeat.new,
     @portal_uri : URI = URI.parse "http://placeos.run"
   )
-    @signature = (secret_key.sign_detached @message.to_json).hexstring
+    @signature = (secret_key.sign_detached @contents.to_json).hexstring
   end
 
   def payload
-    {instance_id: @instance_id, message: @message, signature: @signature}.to_json
+    {instance_id: @instance_id, contents: @contents, signature: @signature}.to_json
   end
 
   def send(custom_uri_path : String? = "") # e.g. /setup
