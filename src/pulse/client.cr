8768976937a5129f0b7664c945ec224fc908c1df
Cameron Reeves <cam@place.technology>
2021-08-26 13:40:57 +0100

    chore(cleanup): basic code cleanup and stop properties being nilable

--- src/pulse/client.cr
+++ src/pulse/client.cr
@@ -1,18 +1,24 @@
+require "tasker"
+require "placeos-models"
+
 module Pulse
   #
   # Pulse::Client will be used to bring together the other aspects of pulse including
   # the Register, Heartbeat and Message classes. It will manage the task of creating heartbeats
   class Client
-    getter instance_id : String?
-    getter private_key : String?
+    getter instance_id : String = ""
+    getter private_key : String = ""
+    getter registered  : Bool = false
     getter task : Tasker::Repeat(HTTP::Client::Response)
 
-    def initialize(@saas : Bool = false, @instance_id=nil, @private_key=nil, heartbeat_interval : Time::Span = 1.day)
+    def initialize(@saas : Bool = false, instance_id=nil, private_key=nil, heartbeat_interval : Time::Span = 1.day)
       # First we need to register the new pulse client in the portal
       # If no ID and private key are passed in then this will be created in the registration class
-      registration = Pulse::Register.new(@saas, @instance_id, @private_key)
-      @instance_id ||= registration.instance_id
-      @private_key ||= registration.private_key
+      registration = Pulse::Register.new(@saas, instance_id, private_key)
+      # Make the call to actually register this instance
+      @registered = registration.portal_request
+      @instance_id = registration.instance_id
+      @private_key = registration.private_key
       @task = Tasker.every(heartbeat_interval) { heartbeat }
     end
 
