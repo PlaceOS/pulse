a07a7ab2b2ba4d86fd09205aa88c6170984c4d99
Cameron Reeves <cam@place.technology>
2021-08-26 13:40:35 +0100

    feat(specs): update specs to use new flow

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,3 +2,9 @@ require "../src/pulse"
 require "spec"
 
 require "webmock"
+
+Spec.before_each &->WebMock.reset
+
+ENV["PORTAL_API_URI"] ||= "https://portal-dev.placeos.run"
+PORTAL_API_URI = ENV["PORTAL_API_URI"]
+ENV["JWT_PRIVATE_KEY"] ||= "<TEST KEY>" # This is just for testing and will be set in core
