0b5b4cc700815cf6876f28f3b01d3650aa9eca95
Cameron Reeves <cam@place.technology>
2021-09-21 18:19:12 +0100

    feat(heartbeat): add logic to count saas features

--- src/pulse/heartbeat.cr
+++ src/pulse/heartbeat.cr
@@ -4,34 +4,35 @@ require "json"
 class Pulse::Heartbeat
   include JSON::Serializable
 
-  getter drivers_qty : Int32
-  getter zones_qty : Int32
-  getter users_qty : Int32
-  getter staff_api : Bool
-  getter instance_type : String
-  getter desks : Array(PlaceOS::Model::Zone)
+  getter desks_qty : Int32
+  getter carparks_qty : Int32
+  getter rooms_qty : Int32
   # add any other telemetry to collect here in future
 
-  def initialize(
-    @drivers_qty = PlaceOS::Model::Driver.count,
-    @zones_qty = PlaceOS::Model::Zone.count,
-    @users_qty = PlaceOS::Model::User.count,
-    # @desks_qty = 
-    @staff_api = true, # figure out how to find this
-    @instance_type = "PROD"
-  ) # and this # maybe an envar...
+  def initialize
+    client = PlaceOS::Client.new(
+      base_uri: ENV["PLACE_URI"],
+      email: ENV["PLACE_EMAIL"],
+      password: ENV["PLACE_PASSWORD"],
+      client_id: ENV["PLACE_AUTH_CLIENT_ID"],
+      client_secret: ENV["PLACE_AUTH_SECRET"],
+      insecure: ENV["PLACE_INSECURE"] || true
+    )
+    @desks_qty = count_features(client, "desks")
+    @carparks_qty = count_features(client, "desks")
+    @rooms_qty = client.systems.search.size
+  end
 
-    # get desks
-    @desks = [] of PlaceOS::Model::Zone
-    # desks << PlaceOS::Model::Zone
-    # @desk_qty =
-    #  (PlaceOS::Model::Zone.count (where type == "desk"))
-    
-    # pp! @drivers_qty
-    
-    # health = PlaceOS::Api::Root.healthcheack?
-    # pp! health
-    # driversreq
-  # add any other telemetry to collect here in future
+  def count_features(client : PlaceOS::Client, metadata_name : String) : Int32
+    # First get all the zones
+    zones = client.zones.search
+    # Now store all the desk objects in any of these zones
+    features = [] of JSON::Any
+    zones.each do |z|
+      feature_metadata = client.metadata.fetch(z.id, metadata_name)
+      features += feature_metadata[metadata_name].details.as_a unless feature_metadata.empty? || !feature_metadata[metadata_name]
+    end
+    # Finally, pass back the total count of desks
+    features.size
   end
 end
