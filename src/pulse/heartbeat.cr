daa5df21da4f2b03c9b30043284293b29896d140
Caspian Baska <email@caspian.computer>
2021-09-28 11:00:39 +1000

    feat(heartbeat): add module type counts

--- src/pulse/heartbeat.cr
+++ src/pulse/heartbeat.cr
@@ -1,22 +1,33 @@
 require "json"
-require "placeos-models/zone"
 require "placeos-models/metadata"
+require "placeos-models/zone"
+require "promise"
 
-class Pulse::Heartbeat
+struct Pulse::Heartbeat
   include JSON::Serializable
 
   enum Feature
     Desks
     Carparks
     Rooms
+
+    def to_json_object_key
+      to_json
+    end
   end
 
-  getter counts : Hash(Feature, Int32)
+  getter feature_count : Hash(Feature, Int32)
 
-  def self.from_database
-    feature_count = Models::Metadata.all.each_with_object(Hash(Feature, Int32).new(0)) do |metadata, count|
+  record ModuleCount, count : Int32, running : Int32 do
+    include JSON::Serializable
+  end
+
+  getter module_instances : Hash(String, ModuleCount)
+
+  def self.feature_count
+    PlaceOS::Model::Metadata.all.each_with_object(Hash(Feature, Int32).new(0)) do |metadata, count|
       # Select for Zone metadata
-      next unless metadata.parent_id.starts_with? Zone.table_name
+      next unless metadata.parent_id.try(&.starts_with? PlaceOS::Model::Zone.table_name)
       # Select for valid feature name
       next unless feature = Feature.parse? metadata.name
 
@@ -30,44 +41,29 @@ class Pulse::Heartbeat
         count[feature] += details.size
       end
     end
-
-    Heartbeat.new(feature_count)
   end
 
-  def self.from_client
-    client = PlaceOS::Client.new(
-      base_uri: PLACE_URI,
-      email: PLACE_EMAIL,
-      password: PLACE_PASSWORD,
-      client_id: PLACE_AUTH_CLIENT_ID,
-      client_secret: PLACE_AUTH_SECRET,
-      insecure: PLACE_INSECURE
-    )
-
-    count = Hash(Feature, Int32).new(0)
-
-    # Fetch _ALL_ Zones
-    client.zones.search.each do |zone|
-      # Extract metadata for desired feature
-      client.metadata.fetch(zone.id).each do |metadata_name, details|
-        # Select for valid feature name
-        next unless feature = Feature.parse?(metadata_name)
-
-        if (feature_array = details.as_a?).nil?
-          Log.warn { {
-            message: "expected an array, got #{details}",
-            zone_id: zone.id,
-            feature: feature.to_s,
-          } }
-        else
-          count[feature] += feature_array.size
-        end
-      end
-    end
-
-    Heartbeat.new(count)
+  def self.module_instances
+    PlaceOS::Model::Module
+      .all
+      .each_with_object(Hash(String, Tuple(Int32, Int32)).new { |h, k| h[k] = {0, 0} }) do |mod, tally|
+        count, running = tally[mod.name]
+        count += 1
+        running += 1 if mod.running
+        tally[mod.name] = {count, running}
+      end.transform_values { |count, running| ModuleCount.new(count, running) }
   end
 
-  def initialize(@counts : Hash(Feature, Int32))
+  def self.from_database
+    # telemetry = Promise.all(
+    #   count = Promise.defer { feature_count },
+    #   modules = Promise.defer { module_instances },
+    # ).get
+    #
+    # Heartbeat.new(*telemetry)
+    Heartbeat.new(feature_count, module_instances)
+  end
+
+  def initialize(@feature_count : Hash(Feature, Int32), @module_instances : Hash(String, ModuleCount))
   end
 end
