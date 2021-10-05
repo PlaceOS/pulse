f804cc5f2f34b480c6da87b24fd947db2f039d59
Caspian Baska <email@caspian.computer>
2021-10-05 21:37:14 +1100

    fix: super instead of raise
    Copy-Paste error

--- src/pulse/error.cr
+++ src/pulse/error.cr
@@ -2,6 +2,6 @@ class PlaceOS::Pulse::Error < Exception
   getter request
 
   def initialize(@request, response)
-    raise("#{request.name} request failed with #{response.status_code}:\n#{response.body}")
+    super("#{request.name} request failed with #{response.status_code}:\n#{response.body}")
   end
 end
