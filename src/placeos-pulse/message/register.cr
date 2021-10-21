b0ab5aa1c4d5f70a0b2f064475007f0c28413a71
Cameron Reeves <cam@place.technology>
2021-10-21 11:20:44 +0100

    feat(register): send public key in register body

--- src/placeos-pulse/message/register.cr
+++ src/placeos-pulse/message/register.cr
@@ -4,17 +4,19 @@ module PlaceOS::Pulse
   struct Message::Register < Request
     getter email : String
     getter proof_of_work : String
+    getter public_key : String
 
     # Generate the register request, performing proof-of-work
     #
     def self.generate(email, instance_id)
       new(
         email,
-        Hashcash.generate(instance_id)
+        Hashcash.generate(instance_id),
+        public_key
       )
     end
 
-    def initialize(@email, @proof_of_work)
+    def initialize(@email, @proof_of_work, @public_key)
     end
   end
 end
