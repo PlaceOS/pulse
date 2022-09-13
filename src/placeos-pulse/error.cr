0dbc07a6e2f2bb5bb52879ab5a2f3f86c9672eb9
Stephen von Takach <steve@place.technology>
2022-09-13 12:28:11 +1000

    fix: token publish only occurs on first launch (#23)
    it now tries to ensure the request succeeds and provide a way of detecting if the request failed.

--- src/placeos-pulse/error.cr
+++ src/placeos-pulse/error.cr
@@ -2,8 +2,11 @@ require "./request"
 
 class PlaceOS::Pulse::Error < Exception
   def initialize(request, response)
+    @token_request = request.is_a?(::PlaceOS::Pulse::Token)
     super("#{request.class} request failed with #{response.status_code}:\n#{response.body}")
   end
+
+  getter? token_request : Bool = false
 
   def initialize(@message)
     super
